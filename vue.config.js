fa7f83ec820dd944d9f4b2c42e090f9172ffb4a5
reda awad <redaawad622@gmail.com>
2021-03-05 20:28:24 +0200

    add android env

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   pwa: {
-    name: 'work-hours',
-    themeColor: '#FFFFFF',
-    msTileColor: '#FFFFFF',
+    name: "work-hours",
+    themeColor: "#FFFFFF",
+    msTileColor: "#FFFFFF",
     manifestOptions: {
-      background_color: '#FFFFFF'
+      background_color: "#FFFFFF"
     }
+  },
+
+  publicPath: "",
+
+  pluginOptions: {
+    cordovaPath: "src-cordova"
   }
-}
+};
